4d14399595a10e3cc7b9a711abdf61df94ae6bb2
Geromendez135135 <136030692+Geromendez135135@users.noreply.github.com>
2023-10-12 23:43:16 -0300

    fds

--- fields_ignition/nodo_test.py
+++ fields_ignition/nodo_test.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 import rospy
+from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Imu
+import time as Time
+
+
+def process_data(data):
+    rospy.loginfo(data)
+    #f = open("data.txt", "x")
+    #f.write(str(data))
+   # f.close()
+    rospy.signal_shutdown("test node has been shutdown")
+
 
 if __name__ == '__main__':
     rospy.init_node('test_node')    
+    sub = rospy.Subscriber("/zed_front/left/compressed", CompressedImage, process_data)
+    #sub2 = rospy.Subscriber("/zed_lateral/zed_lateral/imu/data", Imu, process_data)
+    #sub3 = rospy.Subscriber("/zed_front/zed_node_front/imu/data", Imu, process_data)
+
     rospy.loginfo('test node has been started...')
-
-    rate = rospy.Rate(10)
-
-    while not rospy.is_shutdown(): #si apretamos ctrl c esto se vuelve false 
-        rospy.loginfo('hello')
-        rate.sleep()
+    rospy.spin() #blocks until node is shutdown, Yields activity on other threads
