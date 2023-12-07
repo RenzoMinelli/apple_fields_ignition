fc30d985ed8f57f1c86c0ef5809ea46ced0af956
Geromendez135135 <136030692+Geromendez135135@users.noreply.github.com>
2023-12-06 22:45:38 -0300

    ploteando ahora con tracking de ids

--- fields_ignition/nodo_test_YOLOv8.py
+++ fields_ignition/nodo_test_YOLOv8.py
@@ -18,19 +18,15 @@ def process_data(data):
 
     try:
         global_frame = bridge.compressed_imgmsg_to_cv2(data)
-        results = YOLOv8_model.predict(global_frame, stream=True)
-        print("results: ", results)
-        
-        for result in results:                                         # iterate results
-            boxes = result.boxes.cpu().numpy()                         # get boxes on cpu in numpy
-            for box in boxes:                                          # iterate boxes
-                r = box.xyxy[0].astype(int)                            # get corner points as int
-                print(r)                                               # print boxes
-                cv2.rectangle(global_frame, r[:2], r[2:], (255, 255, 255), 2)   # draw boxes on img
+        #results = YOLOv8_model.predict(global_frame, stream=True)
+        results = YOLOv8_model.track(global_frame, persist=True)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
 
         # Save the image
         current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
-        cv2.imwrite("detected_images_YOLOv8/" + current_time + "_detected.jpg", global_frame)
+        cv2.imwrite("detected_images_YOLOv8/" + current_time + "_detected.jpg", annotated_frame)
     except CvBridgeError as e:
         throw(e)
 
@@ -41,7 +37,8 @@ if __name__ == '__main__':
     YOLOv8_model = YOLO('weights_YOLOv8.pt')
 
     rospy.init_node('test_node')    
-    sub = rospy.Subscriber("/costar_husky_sensor_config_1/left/image_raw/compressed", CompressedImage, process_data, queue_size = 10)
+    #sub = rospy.Subscriber("/costar_husky_sensor_config_1/left/image_raw/compressed", CompressedImage, process_data, queue_size = 10)
+    sub = rospy.Subscriber("/zed_lateral/zed_lateral/left/image_rect_color/compressed", CompressedImage, process_data, queue_size = 10)
     #sub2 = rospy.Subscriber("/zed_lateral/zed_lateral/imu/data", Imu, process_data)
     #sub3 = rospy.Subscriber("/zed_front/zed_node_front/imu/data", Imu, process_data)
 
