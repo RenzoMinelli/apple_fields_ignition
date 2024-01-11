09771fd0e64f48a1670d053a7392409a92529676
PaoloCappelli <paolo.cappelli@fing.edu.uy>
2024-01-11 00:49:51 -0300

    Update save_depth_images.py

--- fields_ignition/nodes/detectors_and_trackers/save_depth_images.py
+++ fields_ignition/nodes/detectors_and_trackers/save_depth_images.py
@@ -1,47 +1,63 @@
 #!/usr/bin/env python3
 
-# imports
 import rospy
-from sensor_msgs.msg import CompressedImage
-from stereo_msgs.msg import DisparityImage
-from sensor_msgs.msg import Imu
-import time as Time
-from ultralytics import YOLO
-import cv2
+import cv2 as cv
+import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
-import torch
-import datetime
-import pdb
-import subprocess
-import os
+from sensor_msgs.msg import Image, CompressedImage
+import message_filters
 
-# callback function
-def process_data(data):
-    try:
-        # debugger
-        pdb.set_trace()
-        global_frame = bridge.imgmsg_to_cv2(data)
-        # save the image with the timestamp of the message, NOT the current time
-        current_time = data.header.sec
-        print("saving image in detected_images_depth_data/" + str(current_time) + "_detected.jpg")
-        cv2.imwrite("detected_images_depth_data/" + str(current_time) + "_detected.jpg", global_frame)
-    except CvBridgeError as e:
-        raise(e)
-
-
-# main function
-if __name__ == '__main__':
-
-    bridge = CvBridge()
-
-    rospy.init_node('test_node')
+def read_cameras():
+    imageL = message_filters.Subscriber("/costar_husky_sensor_config_1/left/image_raw/compressed", CompressedImage)
+    imageR = message_filters.Subscriber("/costar_husky_sensor_config_1/right/image_raw/compressed", CompressedImage)
     
-    # read from simulation
-    sub = rospy.Subscriber("/costar_husky_sensor_config_1/disparity", DisparityImage, process_data, queue_size = 10)
 
-    # read from bag
-    # sub = rospy.Subscriber("/zed_lateral/zed_lateral/left/image_rect_color/compressed", CompressedImage, process_data, queue_size = 10) 
+    # Synchronize images
+    ts = message_filters.ApproximateTimeSynchronizer([imageL, imageR], queue_size=10, slop=0.5)
+    ts.registerCallback(image_callback)
+    rospy.spin()
 
-    rospy.loginfo('test node has been started...')
+def image_callback(imageL, imageR):
+    br = CvBridge()
+    rospy.loginfo("receiving Image")
 
-    rospy.spin() #blocks until node is shutdown, Yields activity on other threads
+    imageLeft = br.compressed_imgmsg_to_cv2(imageL, "rgb8")
+    imageRight = br.compressed_imgmsg_to_cv2(imageR, "rgb8")
+
+    imageL_new=cv.cvtColor(imageLeft, cv.COLOR_BGR2GRAY)
+
+    imageR_new=cv.cvtColor(imageRight, cv.COLOR_BGR2GRAY)
+
+   
+
+    stereo = cv.StereoBM_create(numDisparities=16, blockSize=15)
+    disparity = stereo.compute(imageR_new,imageL_new)
+
+
+
+
+
+
+    rospy.loginfo("showing depth image")
+    # Process images...
+
+    pub = rospy.Publisher('/left_camera', Image, queue_size=1)
+    cv.imshow('disparity', disparity)
+    msg = br.cv2_to_imgmsg(disparity, encoding='16SC1')
+    pub2 = rospy.Publisher('/Right_camera', Image, queue_size=1)
+    msg2 = br.cv2_to_imgmsg(disparity, encoding='16SC1')
+
+    
+    pub.publish(msg)
+    pub2.publish(msg2)
+     
+
+    
+
+if __name__ == '__main__':
+    rospy.init_node('my_node')
+    try:
+        read_cameras()
+        
+    except rospy.ROSInterruptException:
+        pass
