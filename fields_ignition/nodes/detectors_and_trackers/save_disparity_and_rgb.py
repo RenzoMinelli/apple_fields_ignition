d96f51b793a0381768430b21bc59b3bb89b96339
PaoloCappelli <paolo.cappelli@fing.edu.uy>
2024-01-29 23:47:51 -0300

    Nodo casi final

--- fields_ignition/nodes/detectors_and_trackers/save_disparity_and_rgb.py
+++ fields_ignition/nodes/detectors_and_trackers/save_disparity_and_rgb.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import rospy
 import cv2 as cv
 import numpy as np
@@ -9,10 +8,9 @@ from stereo_msgs.msg import DisparityImage
 import message_filters
 
 def read_cameras():
-    #si es la simulacion cambiar /stereo por /costar husky o algo asi, lo que diga el topic de salida de hacer stereo_image_proc
+    #si es la simulacion cambiar /stereo por /costar_husky_sensor_config_1, lo que diga el topic de salida de hacer stereo_image_proc
     imageR = message_filters.Subscriber("/stereo/right/image_rect_color", Image)
-    disparity = message_filters.Subscriber("stereo/disparity", DisparityImage)
-    
+    disparity = message_filters.Subscriber("/stereo/disparity", DisparityImage)
 
     # Synchronize images
     ts = message_filters.TimeSynchronizer([imageR, disparity], queue_size=20)
@@ -27,35 +25,11 @@ def image_callback(imageR, disparity):
     cv_image_right = br.imgmsg_to_cv2(imageR)
     cv_disparity = br.imgmsg_to_cv2(disparity.image)
 
-    seq = imageR.header.seq
+    timestamp = str(imageR.header.stamp)
 
     print("saving images")
-    cv.imwrite('detected_images_depth_data/image_{}.png'.format(seq), cv_disparity)
-    cv.imwrite('detected_images_YOLOv8/image_{}.png'.format(seq), cv_image_right)
-
-
-
-
-
-
-    # rospy.loginfo("showing depth image")
-    # Process images...
-
-    # pub = rospy.Publisher('/left_camera', Image, queue_size=1)
-    # cv.imshow('disparity', disparity)
-    # msg = br.cv2_to_imgmsg(disparity, encoding='16SC1')
-    # pub2 = rospy.Publisher('/Right_camera', Image, queue_size=1)
-    # msg2 = br.cv2_to_imgmsg(disparity, encoding='16SC1')
-    # save the images in the folder detected_images_depth_data
-    # seq = imageL.header.seq
-    # cv.imwrite('detected_images_depth_data/disparity_{}.png'.format(seq), disparity)
-
-    
-    # pub.publish(msg)
-    # pub2.publish(msg2)
-     
-
-    
+    cv.imwrite('detected_images_depth_data/{}.png'.format(timestamp), cv_disparity)
+    cv.imwrite('detected_images_YOLOv8/{}.png'.format(timestamp), cv_image_right)
 
 if __name__ == '__main__':
     rospy.init_node('my_node')
@@ -66,9 +40,19 @@ if __name__ == '__main__':
         pass
 
 
+# Bag de mercedes:
+# en fields_ignition/launch/stereo.launch cambiar el path del bag en la linea 3 en args
 # roslaunch fields_ignition stereo.launch
 # ROS_NAMESPACE=stereo rosrun stereo_image_proc stereo_image_proc
-# rosrun rqt_reconfigure rqt_reconfigure y lodear el yaml en /home/paolo/catkin_ws/src/apple_fields_ignition/fields_ignition/config/stereoConfigMercedez.yaml
-# rosrun image_view stereo_view stereo:=/stereo image:=image_rect_color para verificacion visual de que se ve bien]
+# Si queres mejor calidad: rosrun rqt_reconfigure rqt_reconfigure y cargar el yaml en /home/paolo/catkin_ws/src/apple_fields_ignition/fields_ignition/config/stereoConfigMercedez.yaml
+# rosrun image_view stereo_view stereo:=/stereo image:=image_rect_color (para verificacion visual de que se ve bien)
 # rosrun fields_ignition save_disparity_and_rgb.py
-# no se xq por un momento se veian bien y despues se ven medias negras, tengo que pasarle los args del yaml a stereo_image_proc
+
+# Simulacion:
+# cambiar en este py lo que inica el comentario en la linea 12
+# roslaunch fields_ignition fields_ignition.launch
+# ROS_NAMESPACE=costar_husky_sensor_config_1 rosrun stereo_image_proc stereo_image_proc
+# rosrun image_view stereo_view stereo:=/stereo image:=image_rect_color
+# Si queres mejor calidad: rosrun rqt_reconfigure rqt_reconfigure y cargar el yaml en /home/paolo/catkin_ws/src/apple_fields_ignition/fields_ignition/config/stereoConfigSim.yaml
+# rosrun image_view stereo_view stereo:=/stereo image:=image_rect_color (para verificacion visual de que se ve bien)
+# rosrun fields_ignition save_disparity_and_rgb.py
