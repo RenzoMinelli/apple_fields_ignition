41c6f1fb0cb4d88c6db19be4888461fcc6e845a4
Geromendez135135 <136030692+Geromendez135135@users.noreply.github.com>
2024-07-20 21:51:47 -0300

    placeholders

--- fields_ignition/nodes/detectors_and_trackers/filtrado/sin_filtrado.py
+++ fields_ignition/nodes/detectors_and_trackers/filtrado/sin_filtrado.py
@@ -3,5 +3,5 @@
 from . import filtrado_base
 
 class SinFiltrado(filtrado_base.FiltradoBase): 
-    def filter(self, timestamp, bounding_boxes, img_original, mapa_profundidad):
+    def filter(self, _1, bounding_boxes, _2, _3):
         return bounding_boxes
