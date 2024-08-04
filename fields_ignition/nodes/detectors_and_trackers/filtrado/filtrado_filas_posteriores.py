dd6f25bc23be8459eef40f042c2ae2ff65e9d78f
Renzo Minelli <renminelli@gmail.com>
2024-08-04 00:50:31 -0300

    mover constate a configuracion

--- fields_ignition/nodes/detectors_and_trackers/filtrado/filtrado_filas_posteriores.py
+++ fields_ignition/nodes/detectors_and_trackers/filtrado/filtrado_filas_posteriores.py
@@ -4,9 +4,11 @@ from . import filtrado_base
 import numpy as np
 from sklearn.cluster import KMeans
 
-DISTANCIA_FILTRO = 3 # metros
-
 class FiltradoFilasPosteriores(filtrado_base.FiltradoBase):
+    def __init__(self, config={}):
+        super().__init__(config)
+        self.FiltradoFilas_DISTANCIA_FILTRO = self.imported_config.getfloat('FILTRADO_FILAS_POSTERIORES', 'distancia_filtro')
+
     def filter(self, _1, bounding_boxes, _2, mapa_profundidad):
         return self.__filtrar_puntos(bounding_boxes, mapa_profundidad)
 
@@ -14,7 +16,7 @@ class FiltradoFilasPosteriores(filtrado_base.FiltradoBase):
         return super().obtener_puntos_con_profunidad(bounding_boxes, mapa_profundidad)
 
     def __filtrar_puntos_threshold(self, puntos):
-        return [p for p in puntos if p[2] < DISTANCIA_FILTRO]
+        return [p for p in puntos if p[2] < self.FiltradoFilas_DISTANCIA_FILTRO]
 
     def __quitar_profunidad(self, puntos):
         return [[x,y,*rest] for [x,y,z,*rest] in puntos]
