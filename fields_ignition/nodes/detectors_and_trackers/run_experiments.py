c9fbed1ff28b3523af5c99d9f9b63317ff073e40
Renzo Minelli <renminelli@gmail.com>
2024-08-15 00:09:42 -0300

    soportar todos los ambientes

--- fields_ignition/nodes/detectors_and_trackers/run_experiments.py
+++ fields_ignition/nodes/detectors_and_trackers/run_experiments.py
@@ -1,4 +1,7 @@
 import subprocess
+import os 
+
+CWD = os.getcwd()
 
 CONFIG_FILES = [
     "plano.ini",
@@ -8,11 +11,14 @@ CONFIG_FILES = [
 ]
 
 if __name__ == "__main__":
+    my_env = os.environ.copy()
+    my_env["PYTHONPATH"] = f"{CWD}/src/apple_fields_ignition/fields_ignition/nodes"
+
     for config_file in CONFIG_FILES:
         subprocess.run([
             "python",
             "-m", 
             "detectors_and_trackers.track_and_filter", 
             "--config", 
-            f"/home/renzo/catkin_ws/src/apple_fields_ignition/fields_ignition/nodes/experiments_configs/{config_file}"
-        ])
+            f"{CWD}/src/apple_fields_ignition/fields_ignition/nodes/detectors_and_trackers/experiments_configs/{config_file}"
+        ], env=my_env)
